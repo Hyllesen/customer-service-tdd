9dbb14d395baa94d7a990de434316c8471d8da26
stefan <stefan@stefans-iMac.local>
2019-08-08 17:17:16 +0200

    Wrote first tests

---
+++ tests/unit/mongodb/mongodb.module.specs.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const MongoDBModule = require("../../../modules/mongodb/mongodb.module");
+
+describe("MongoDBModule", () => {
+  describe("mongodb.module file", () => {
+    it("should describe the test and it's expectation", () => {});
+  });
+});
---
+++ tests/unit/mongodb/mongodb.util.specs.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const MongoDBUtil = require("../../../modules/mongodb/mongodb.util");
+
+describe("MongoDBUtil", () => {
+  describe("mongodb.util file", () => {
+    it("should test first behaiviour", () => {
+      //Write expectations for first behaivor here
+    });
+  });
+});
