075e060a0909d18942f26efcae936c2258e60bc3
stefan <stefan@stefans-iMac.local>
2019-08-08 21:12:50 +0200

    Added fixtures

---
+++ tests/fixtures/customer/customer-fixture.js
@@ -0,0 +1,9 @@
+(() => {
+  "use strict";
+  module.exports = {
+    customers: require("./customers.json"),
+    newCustomers: require("./new-customer.json"),
+    createdCustomer: require("./created-customer.json"),
+    modifiedCustomer: require("./modified-customer.json")
+  };
+})();
